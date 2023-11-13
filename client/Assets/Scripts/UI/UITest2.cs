1294c08793c90f82c7a211611a28ed06bcdfbfe3
Xoen <15290725@qq.com>
2023-11-13 09:08:43 +0800

    修正UI模板代码

--- client/Assets/Scripts/UI/UITest2.cs
+++ client/Assets/Scripts/UI/UITest2.cs
@@ -1,29 +1,33 @@
 //////////////////////////////////////////////////////////////////////////
 //
-// 文件：Assets\Scripts\UI\_UI_NAME_.cs
+// 文件：Assets\Scripts\UI\UITest2.cs
 // 作者：Xoen Xie
-// 时间：_LUA_DATE_
+// 时间：11/13/2023
 // 描述：
 // 说明：
 //
 //////////////////////////////////////////////////////////////////////////
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using XSF;
 
 namespace XsfUI
 {
 
-public sealed class _UI_NAME_ : UIBase
+public sealed class UITest2 : UIBase
 {
 // UI_PROP_START
+	public TextMeshProUGUI TestTxt { get; private set; }	// 测试文本
 // UI_PROP_END
 
-    public override string Name { get { return "_UI_NAME_"; } }
+    public override string Name { get { return "UITest2"; } }
 
     public override void OnInit()
     {
         // UI_INIT_START
+		// 测试文本
+		TestTxt = RootT.Find("Image/Text (TMP)").GetComponent<TextMeshProUGUI>();
         // UI_INIT_END
     }
 
